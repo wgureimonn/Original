29a0262f2c0753f71ca2e937cb9e39b33c247362
LD10-MIZUKI-MAS\wgure <git@github.com:wgureimonn/unity.git>
2018-04-28 12:56:40 +0900

    素材

---
+++ Assets/dungeonmaker.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dungeonmaker : MonoBehaviour {
+int x;
+int z;
+public GameObject zimen;
+public GameObject kabesita;
+public GameObject kabeue;
+public GameObject kabemigi;
+public GameObject kabehidari;
+public GameObject kabekado;
+public GameObject floor;
+public GameObject wall;
+
+	// Use this for initialization
+	void Start () {
+		for(x=0;x<10;x++){
+			for(z=0;z<10;z++){
+				GameObject floorc = Instantiate(zimen,new Vector3(x,0,z),Quaternion.identity);
+				floorc.transform.parent = floor.transform;
+			}
+		}
+		for(x=-1;x<11;x++){
+			for(z=-1;z<11;z++){
+				if((x==10)&&((z==10)||(z==-1))||((x==-1)&&((z==10)||(z==-1)))){
+					GameObject wallc = Instantiate(kabekado,new Vector3(x,0,z),Quaternion.identity);
+					wallc.transform.parent = wall.transform;
+				}
+				else if(z==-1){
+					GameObject wallc = Instantiate(kabesita,new Vector3(x,0,z),Quaternion.identity);
+					wallc.transform.parent = wall.transform;
+				}
+				else if(z==10){
+					GameObject wallc = Instantiate(kabeue,new Vector3(x,0,z),Quaternion.identity);
+					wallc.transform.parent = wall.transform;
+				}
+				else if(x==10){
+					GameObject wallc = Instantiate(kabehidari,new Vector3(x,0,z),Quaternion.identity);
+					wallc.transform.parent = wall.transform;
+				}
+				else if(x==-1){
+					GameObject wallc = Instantiate(kabemigi,new Vector3(x,0,z),Quaternion.identity);
+					wallc.transform.parent = wall.transform;
+				}
+			}
+		}
+		
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
