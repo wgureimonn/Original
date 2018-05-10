72eacf4faade1bc82c14052f7777e9195ecc64ea
LD10-MIZUKI-MAS\wgure <git@github.com:wgureimonn/unity.git>
2018-05-10 15:28:13 +0900

    前回の提出からの更新点 ・配列の生成のバグの改善 ・敵AIの搭載 　・現在地点からいける場所へ移動する 　・分岐点はランダム 　・行き止まりにあたるとそこから再計算し元の道へ戻る 　・ゴールから上下7マス以内に入るとゴールまでの最短経路を読み込みその順にたどっていく

--- Assets/code/wallmaker.cs
+++ Assets/code/wallmaker.cs
@@ -1,131 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class wallmaker : MonoBehaviour {
-	int [,] map=new int[10,10];
-    int [,] OC =new int[10,10];
-    int [,] SI =new int[10,10];
-    int [,] C  =new int[10,10];
-    int [,] SC =new int[10,10];
-    int Gx=3;
-    int Gz=4;
-    int cc=1;
-    int cp;
+	public static int[,] map;
 
-	int x;
-    int xa;
-	int z=0;
-    int za;
 	public GameObject wall;
 	public GameObject wallu;
     GameObject wallc;
     GameObject wallk;
+    int x;
+    int xa;
+	int z;
+    int za;
 	// Use this for initialization
-	void Start ()
-    {
+    void Awake(){
+		map=new int[10,10];
+
         wallmake();
         wallumake();
-        openclose();
-        suitei();
-        cost();
-        score();
-        for (x = 0; x < 10; x++)
-        {
-            for (z = 0; z < 10; z++)
-            {
-                Debug.Log(SC[x, z]);
-            }
-        }
         
         
-    }
-
-    private void score()
-    {
-        for (x = 0; x < 10; x++)
-        {
-            for (z = 0; z < 10; z++)
-            {
-                if (OC[x, z] == 0||OC[x,z]==1)
-                {
-                    SC[x,z]=SI[x,z]+C[x,z];
-                }
-            }
-        }
-    }
-
-    private void cost(){
-        while(true){
-            cp=0;
-            for (x = 0; x < 10; x++)
-            {
-                for (z = 0; z < 10; z++)
-                {
-                    
-                    if (C[x,z]==cc)
-                    {
-                        if(OC[x,z+1]==0){
-                            C[x,z+1] =C[x,z]+1;
-                            OC[x,z+1]=1;
-                            cp=1;
-                       }
-                        if(OC[x+1,z]==0){
-                            C[x+1,z] =C[x,z]+1;
-                            OC[x+1,z]=1;
-                            cp=1;
-                        }
-                        if(OC[x,z-1]==0){
-                            C[x,z-1] =C[x,z]+1;
-                            OC[x,z-1]=1;
-                            cp=1;
-                       }
-                        if(OC[x-1,z]==0){
-                            C[x-1,z] =C[x,z]+1;
-                            OC[x-1,z]=1;
-                            cp=1;
-                        }
-                    }
-                }
-            }
-            cc++;
-            if(cp==0)break;
-        }
-    }
-
-    private void suitei()
-    {
-        for (x = 0; x < 10; x++)
-        {
-            for (z = 0; z < 10; z++)
-            {
-                if(map[x,z]==0){
-                    SI[x,z]=Mathf.Abs(Gx-x)+Mathf.Abs(Gz-z);
-                }
-            }
-        }
-    }
-
-    private void debugg()
-    {
-        for (x = 0; x < 10; x++)
-        {
-            for (z = 0; z < 10; z++)
-            {
-                Debug.Log(map[x, z]);
-            }
-        }
-    }
-
-    private void openclose()
-    {
-        for (x = 0; x < 10; x++)
-        {
-            for (z = 0; z < 10; z++)
-            {
-                OC[x,z]=map[x,z];
-            }
-        }
     }
 
     private void wallumake(){
@@ -143,7 +38,7 @@ public class wallmaker : MonoBehaviour {
         for (x = 0; x < 5; x++){
             for(z=0;z<9;z++){
                 xa=x*2;
-                za=z+1-x%2;
+                za=z+1;
                 Instantiate(wall, new Vector3(xa, 1, za), Quaternion.identity);
                 map[xa, za] = 2;
             }
--- Assets/enemyAI.cs
+++ Assets/enemyAI.cs
@@ -2,29 +2,268 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyAI : MonoBehaviour {
-	int now;
-	int score;
-	int suitei;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown("up")){
-			transform.position -= new Vector3(1,0,0);
-		}
-		if(Input.GetKeyDown("down")){
-			transform.position += new Vector3(1,0,0);
-		}
-		if(Input.GetKeyDown("right")){
-			transform.position += new Vector3(0,0,1);
-		}
-		if(Input.GetKeyDown("left")){
-			transform.position -= new Vector3(0,0,1);
-		}
-		
-	}
+public class enemyAI : MonoBehaviour
+{
+    int now=0;
+    int g=1;
+    public static int[,] OC;
+    public static int[,] SI;
+    public static int[,] C;
+    public static int[,] SC;
+    int Gx = 10;
+    int Gz = 10;
+    int cc = 1;
+    int cp;
+    int sui;
+    private Vector2Int pos;
+    private Vector2Int ipos;
+    private Vector2Int kari;
+    private List<Vector2Int> rout;
+
+    int x;
+    int xa;
+    int z = 0;
+    int za;
+    // Use this for initialization
+    void Start()
+    {
+        pos = new Vector2Int(0, 0);
+        openclose();
+        suitei();
+        cost();
+        score();
+        InvokeRepeating("idou", 1, 0.5f);
+    }
+    private void score()
+    {
+        SC = new int[10, 10];
+        for (x = 0; x < 10; x++)
+        {
+            for (z = 0; z < 10; z++)
+            {
+                if (OC[x, z] == 0 || OC[x, z] == 1)
+                {
+                    SC[x, z] = SI[x, z] + C[x, z];
+                }
+            }
+        }
+    }
+
+    private void cost()
+    {
+        C = new int[10, 10];
+        for (x = 0; x < 10; x++)
+        {
+            for (z = 0; z < 10; z++)
+            {
+                if ((x == pos.x) && (z == pos.y) && (OC[x, z] == 0))
+                {
+                    C[x, z] = 0;
+                    OC[x, z] = 1;
+                    if (z < 9)
+                    {
+                        if (OC[x, z + 1] == 0)
+                        {
+                            C[x, z + 1] = C[x, z] + 1;
+                            OC[x, z + 1] = 1;
+                            cp = 1;
+                        }
+                    }
+                    if (x < 9)
+                    {
+                        if (OC[x + 1, z] == 0)
+                        {
+                            C[x + 1, z] = C[x, z] + 1;
+                            OC[x + 1, z] = 1;
+                            cp = 1;
+                        }
+                    }
+                    if (z > 0)
+                    {
+                        if (OC[x, z - 1] == 0)
+                        {
+                            C[x, z - 1] = C[x, z] + 1;
+                            OC[x, z - 1] = 1;
+                            cp = 1;
+                        }
+                    }
+                    if (x > 0)
+                    {
+                        if (OC[x - 1, z] == 0)
+                        {
+                            C[x - 1, z] = C[x, z] + 1;
+                            OC[x - 1, z] = 1;
+                            cp = 1;
+                        }
+                    }
+                }
+            }
+        }
+        cc = 1;
+        while (true)
+        {
+            cp = 0;
+            for (x = 0; x < 10; x++)
+            {
+                for (z = 0; z < 10; z++)
+                {
+                    if (C[x, z] == cc)
+                    {
+                        if (z < 9)
+                        {
+                            if (OC[x, z + 1] == 0)
+                            {
+                                C[x, z + 1] = C[x, z] + 1;
+                                OC[x, z + 1] = 1;
+                                cp = 1;
+                            }
+                        }
+                        if (x < 9)
+                        {
+                            if (OC[x + 1, z] == 0)
+                            {
+                                C[x + 1, z] = C[x, z] + 1;
+                                OC[x + 1, z] = 1;
+                                cp = 1;
+                            }
+                        }
+                        if (z > 0)
+                        {
+                            if (OC[x, z - 1] == 0)
+                            {
+                                C[x, z - 1] = C[x, z] + 1;
+                                OC[x, z - 1] = 1;
+                                cp = 1;
+                            }
+                        }
+                        if (x > 0)
+                        {
+                            if (OC[x - 1, z] == 0)
+                            {
+                                C[x - 1, z] = C[x, z] + 1;
+                                OC[x - 1, z] = 1;
+                                cp = 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (cp == 0)
+            {
+                break;
+            }
+            cc = cc + 1;
+        }
+
+    }
+    private void suitei()
+    {
+        SI = new int[10, 10];
+        for (x = 0; x < 10; x++)
+        {
+            for (z = 0; z < 10; z++)
+            {
+                if (wallmaker.map[x, z] == 0)
+                {
+                    SI[x, z] = Mathf.Abs(Gx - x) + Mathf.Abs(Gz - z);
+                }
+            }
+        }
+
+    }
+    private void openclose()
+    {
+        OC = new int[10, 10];
+        for (x = 0; x < 10; x++)
+        {
+            for (z = 0; z < 10; z++)
+            {
+                OC[x, z] = wallmaker.map[x, z];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            transform.position -= new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            transform.position += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            transform.position -= new Vector3(0, 0, 1);
+        }
+
+    }
+    void idou()
+    {
+        if (now == 0 && SI[pos.x, pos.y] <= 10)
+        {
+            now = 1;
+        }
+        if (now == 1)
+        {
+            goal();
+            now = 2;
+            g=-1;
+        }
+        else
+        {
+            round();
+        }
+        transform.position = new Vector3(pos.x, 1, pos.y);
+
+    }
+    void round()
+    {
+        List<Vector2Int> houkou = new List<Vector2Int>();
+        if ((pos.y < 9) && (C[pos.x, pos.y + 1] == C[pos.x, pos.y] + g))
+        {
+            houkou.Add(Vector2Int.up);
+        }
+        if ((pos.x < 9) && (C[pos.x + 1, pos.y] == C[pos.x, pos.y] + g))
+        {
+            houkou.Add(Vector2Int.right);
+        }
+        if ((pos.x > 0) && (C[pos.x - 1, pos.y] == C[pos.x, pos.y] + g))
+        {
+            houkou.Add(Vector2Int.left);
+        }
+        if ((pos.y > 0) && (C[pos.x, pos.y - 1] == C[pos.x, pos.y] + g))
+        {
+            houkou.Add(Vector2Int.down);
+        }
+        if (houkou.Count == 0)
+        {
+            openclose();
+            cost();
+            score();
+        }
+        else
+        {
+            pos += houkou[Random.Range(0, houkou.Count)];
+        }
+    }
+    void goal()
+    {
+        ipos.x=pos.x;
+        ipos.y=pos.y;
+        pos.x=Gx-1;
+        pos.y=Gz-1;
+        openclose();
+        cost();
+        score();
+        pos.x=ipos.x;
+        pos.y=ipos.y;
+    }
 }
